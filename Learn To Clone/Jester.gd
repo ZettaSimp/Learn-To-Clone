26d56e198ae4948eacfed74714d4f7c81fecf399
ZettaSimp <156013140+ZettaSimp@users.noreply.github.com>
2024-09-28 10:31:08 -0500

    Heck

--- Learn To Clone/Jester.gd
+++ Learn To Clone/Jester.gd
@@ -6,6 +6,8 @@ var speed = [0,0]
 var bounce_constant = 0.6
 var floor_constant = 0.9
 var drag = 0.25
+var glider_constant = 0.9
+var glide = [0,0]
 var bounces = 0
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -13,6 +15,7 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	glider_constant = 0.8 * delta
 	if Input.is_action_pressed('ui_left') == true:
 		Jest.rotation_degrees = Jest.rotation_degrees - delta * 90
 	if Input.is_action_pressed('ui_right') == true:
@@ -20,6 +23,8 @@ func _process(delta):
 	if Input.is_action_pressed('ui_accept') == true:
 		speed[0] += accel * sin(Jest.rotation) * delta
 		speed[1] += accel * -cos(Jest.rotation) * delta
+	if glider_constant > 0:
+		glide = [sqrt(speed[0]*speed[0]+speed[1]*speed[1]),sqrt(speed[0]*speed[0]+speed[1]*speed[1])]
 	speed = [speed[0]-speed[0]*drag*delta,speed[1]-speed[1]*drag*delta]
 	global_position.x += speed[0] * delta
 	if global_position.y + speed[1] * delta < 455:
